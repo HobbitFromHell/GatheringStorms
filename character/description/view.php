e138d5f0cfa6e34086c3723ba19087acd62b5f37
myronwyles <myronwyles@gmail.com>
2015-01-27 11:01:34 -0800

    Create view.php
    Transferred from CVS 20150127

---
+++ character/description/view.php
@@ -0,0 +1,26 @@
+<?php
+
+// Description section
+$output = new BuildOutput("Description");
+
+// id
+$output->add("id", $pkid, 0, 0);
+
+// description
+$output->add("", "<div class=\"statBlockDetail\">", "", "", "");
+$output->add("description", $view->characterDescription[description][name], "", "", "textarea");
+$output->add("", "</div>", "", "", "");
+$output->br();
+
+// location
+$output->add("location", $view->characterDescription[description][location], "Location", "Location", "200");
+if($view->characterDescription[description][loc_name]) {
+	$output->addRead("(" . $view->characterDescription[description][loc_name] . ")");
+}
+$output->br();
+
+echo $output->dump(1);
+
+echo drawHexMap($varXCo, $varYCo, $varHexSize, $view->characterDescription[location]);
+
+?>

3937a2888d758d873630ec1a3ace3ac0928672ac
myronwyles <myronwyles@gmail.com>
2015-01-27 11:09:52 -0800

    Create controller.php
    Transferred from CVS 20150127

---
+++ character/specialabilities/controller.php
@@ -0,0 +1,29 @@
+<?php
+
+// //////////
+// validation
+// //////////
+
+if (!($pkid > 0 and $pkid < 65000)) {
+	echo "FAIL: Character ID {$pkid} is invalid";
+	return false;
+}
+
+
+// //////
+// insert
+// //////
+
+post_data("t_characters_racial_traits", "id", "character_id", Array(Array("racialtrait", "racial_trait_id"), "detail"));
+post_data("t_characters_character_classes_class_features", "id", "character_id", Array(Array("classfeature", "character_Classes_class_feature_id"), "detail"));
+
+
+// ////////////////////////////
+// communicate with parent page
+// ////////////////////////////
+
+echo "<script>\n";
+echo "buildSection('Feats', 'total_level=' + charSheet.total_level + '&race=' + charSheet.race + '&total_bab=' + charSheet.total_bab + '&str=' + charSheet.str + '&dex=' + charSheet.dex + '&con=' + charSheet.con + '&int=' + charSheet.int + '&wis=' + charSheet.wis + '&cha=' + charSheet.cha)\n";
+echo "</script>\n";
+
+?>

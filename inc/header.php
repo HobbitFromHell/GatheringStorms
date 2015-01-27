d57bdd042199c7355d75dd7f8519414210375cea
myronwyles <myronwyles@gmail.com>
2015-01-27 10:52:31 -0800

    Create header.php
    Transferred from CVS 20150127

---
+++ inc/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gathering Storms: Characters</title>
+	<link href="/inc/gatheringstorms.css" type="text/css" rel="stylesheet"/>
+	<script>var pkid = "<?php if(isset($pkid)) echo ($pkid); else echo ("0"); ?>"</script>
+	<script src="/inc/jquery-2.1.1.min.js"></script>
+	<script src="/inc/gatheringstorms.js"></script>
+</head>
+<body onLoad="start('<?= $pkid ?>')">
+	<table class="statBlockDoubleSection" style="background-color: #000; border: none; margin-left: 40px;">
+		<tr>
+			<td align="center">
+				<img src="/img/Forgotten Realms Logo.gif">
+			</td>
+		</tr>
+	</table>
+	<br>
+	<table class="statBlockDoubleSection" style="margin-left: 41px;">
+		<tr>
+			<td align="center"><a class="menuLink" href="/character/">People</a></td>
+			<td align="center"><a class="menuLink" href="/location/">Places</a></td>
+			<td align="center"><a class="menuLink" href="/encounter/">Events</a></td>
+		</tr>
+	</table>
+	<br>

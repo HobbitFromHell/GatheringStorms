6b849f11ad5f348b65e954a9bef82235d3eab548
myronwyles <myronwyles@gmail.com>
2015-01-27 11:34:39 -0800

    Create view.php
    Transferred from CVS 20150127

---
+++ location/encounter/view.php
@@ -0,0 +1,28 @@
+<?php
+
+// cast section
+$output = new BuildOutput("Encounter");
+
+// id
+$output->add("id", $pkid, 0, 0);
+
+$output->addEdit("</form>");
+$output->addEdit("<form action=\"/encounter/add/?id=0\" method=\"POST\" id=\"addNewLocalEncounter\">");
+$output->addEdit("<input type=\"hidden\" name=\"name\" value=\"\">");
+$output->addEdit("<input type=\"hidden\" name=\"favoured_class\" value=\"COM\">");
+$output->addEdit("<input type=\"hidden\" name=\"chapter\" value=\"0\">");
+$output->addEdit("<input type=\"hidden\" name=\"location_id\" value=\"{$pkid}\">");
+$output->addEdit("<input type=\"submit\" name=\"create\" value=\"Create New Encounter Here\" onClick=\"document.getElementById('addNewLocalEncounter').submit();\">");
+
+// build character list
+//unset($view->encounterCast['list']);
+if($view->locationEncounter[0]) {
+	$output->addRead(buildEncList($view->locationEncounter));
+}
+else {
+	$output->br();
+}
+
+echo $output->dump(1);
+
+?>

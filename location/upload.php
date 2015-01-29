bb1efa2776cc0af9c04a742533b35a38511ae473
myronwyles <myronwyles@gmail.com>
2015-01-29 15:43:46 -0800

    Update upload.php
    Custom alert warnings to narrow down errors.

--- location/upload.php
+++ location/upload.php
@@ -4,7 +4,7 @@ $target_dir = "img/";
 $target_ext = basename( $_FILES["fileToUpload"]["name"] );
 $uploadOk = 1;
 $errMessage = "";
-$imageFileType = pathinfo($_FILES["fileToUpload"]["name"], PATHINFO_EXTENSION);
+$imageFileType = strtolower(pathinfo($_FILES["fileToUpload"]["name"], PATHINFO_EXTENSION));
 $target_file = $target_dir . "map" . $_GET[id] . "." . $imageFileType;
 
 // Check if image file is a actual image or fake image
@@ -12,6 +12,7 @@ if(isset($_GET[id])) {
 	$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
 	if($check == false) {
 		$uploadOk = 0;
+		echo("<script>alert('Sorry, only images may be uploaded.');</script>");
 	}
 
 	// Check if file already exists
@@ -22,19 +23,18 @@ if(isset($_GET[id])) {
 	// Check file size
 	if ($_FILES["fileToUpload"]["size"] > 500000) {
 		$uploadOk = 0;
+		echo("<script>alert('Sorry, your file is too large to be uploaded.');</script>");
 	}
 	
 	// Allow certain file formats
 	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
 		$uploadOk = 0;
+		echo("<script>alert('Sorry, only files with the extension JPG, JPEG, PNG and GIF can be uploaded.');</script>");
 	}
 
 	// Check if $uploadOk is set to 0 by an error
 	// if everything is ok, try to upload file
-	if ($uploadOk == 0) {
-		echo("<script>alert('Sorry, your file was not uploaded.');</script>");
-	}
-	else {
+	if ($uploadOk) {
 		if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
 			if ($uploadOk == 2) {
 				// replacement image uploaded, requires refresh of window

7ec56fe24d27a4394d22e643c7510b0233a53f26
myronwyles <myronwyles@gmail.com>
2015-02-07 10:51:45 -0800

    Reformat layout
    Separate read-only and edit sections:
    Organize read-only as standard stat block, with combat gear, other gear, plus new mount gear and stored gear options which do not effect encumbrance.
    Organize edit section into weapons (melee, off-hand and ranged), armour (armour and shields), and other. is_equipped can select aforementioned options plus yes, no, mount and stored.

--- character/treasure/view.php
+++ character/treasure/view.php
@@ -9,42 +9,41 @@ $output->add("size", $view->characterTreasure[param][size], 0, 0);
 $output->add("total_value", $view->total_value, 0, 0);
 $output->add("total_weight", $view->total_weight, 0, 0);
 
-// summary
-$output->addEdit("<b>Total Load</b> " . $view->total_weight . " lbs. <span id=\"calcLoad\"></span><br>", "");
-$output->addEdit("<b>Total Value</b> " . $view->total_value / 100 . " <span id=\"calcMaxValue\"></span> gp", "");
+// summary (read-only)
+$output->addRead("<b>Total Load</b> " . $view->total_weight . " lbs. <span id=\"calcLoad\"></span><br>", "");
+$output->addRead("<b>Total Value</b> " . number_format($view->total_value / 100) . " <span id=\"calcMaxValue\"></span> gp", "");
+$output->addRead("<br>");
 
 // combat gear (read-only assembly of all equipped gear)
-$output->add("", $combatGear, "Combat Gear", 0, 0);
-$output->br();
+if($view->combatGear) {
+	$output->add("", $view->combatGear, "Combat Gear", 0, 0);
+	$output->addRead("<br>");
+}
+
+// other gear (read-only assembly of all unequipped gear)
+if($view->otherGear) {
+	$output->add("", $view->otherGear, "Other Gear", 0, 0);
+	$output->addRead("<br>");
+}
+
+// mount gear (read-only assembly of all gear on the mount)
+if($view->mountGear) {
+	$output->add("", $view->mountGear, "Mount Gear", 0, 0);
+	$output->addRead("<br>");
+}
+
+// stored gear (read-only assembly of all stored gear)
+if($view->storedGear) {
+	$output->add("", $view->storedGear, "Stored Gear", 0, 0);
+	$output->addRead("<br>");
+}
 
 // melee weapon (edit only)
-$output->add("melee", $view->characterTreasure[melee], 0, "<b>Melee Weapon</b><br>", "100",
+$output->add("weapon", $view->characterTreasure[weapon], 0, "<b>Weapons</b><br>", "100",
 	array("DELETE", "; ", 
-		array("quality", "", "", "70"), 
-		array("repair", 0,  "", "60", 0, array("Good", "Worn", "Broken")), 
-		array("is_equipped", 0, "", "60", "Melee", array("Melee", "Off-Hand", "Ranged", "No")), 
-		array("is_deleted", 0, 0, 0)
-	)
-);
-$output->addEdit("<br>");
-
-// off-hand weapon (edit only)
-$output->add("offhand", $view->characterTreasure[offhand], 0, "<b>Off-Hand Weapon</b><br>", "100",
-	array("DELETE", "; ", 
-		array("quality", "", "", "70"), 
-		array("repair", 0, "", "60", 0, array("Good", "Worn", "Broken")), 
-		array("is_equipped", 0, "", "60", "Off-Hand", array("Melee", "Off-Hand", "Ranged", "No")), 
-		array("is_deleted", 0, 0, 0)
-	)
-);
-$output->addEdit("<br>");
-
-// ranged weapon (edit only)
-$output->add("ranged", $view->characterTreasure[ranged], 0, "<b>Ranged Weapon</b><br>", "100",
-	array("DELETE", "; ", 
-		array("quality", "", "", "70"), 
-		array("repair", 0, "", "60", 0, array("Good", "Worn", "Broken")), 
-		array("is_equipped", 0, "", "60", "Ranged", array("Melee", "Off-Hand", "Ranged", "No")), 
+		array("quality", "", "", "90"), 
+		array("count", "", "", "30"), 
+		array("is_equipped", 0, "", "72", "", array("Melee", "Off-Hand", "Ranged", "No", "Mount", "Storage")), 
 		array("is_deleted", 0, 0, 0)
 	)
 );
@@ -53,46 +52,23 @@ $output->addEdit("<br>");
 // armour (edit only)
 $output->add("armour", $view->characterTreasure[armour], 0, "<b>Armour</b><br>", "100",
 	array("DELETE", "; ", 
-		array("quality", "", "", "70"), 
-		array("repair", 0, "", "60", 0, array("Good", "Worn", "Broken")), 
-		array("is_equipped", 0, "", "60", "Armour", array("Armour", "No")), 
-		array("is_deleted", 0, 0, 0)
-	)
-);
-$output->addEdit("<br>");
-
-// shield (edit only)
-$output->add("shield", $view->characterTreasure[shield], 0, "<b>Shield</b><br>", "100",
-	array("DELETE", "; ", 
-		array("quality", "", "", "70"), 
-		array("repair", 0, "", "60", 0, array("Good", "Worn", "Broken")), 
-		array("is_equipped", 0, "", "60", "Armour", array("Armour", "Off-Hand", "No")), 
-		array("is_deleted", 0, 0, 0)
-	)
-);
-$output->addEdit("<br>");
-
-// other combat gear (edit only)
-$output->add("othercombat", $view->characterTreasure[othercombat], 0, "<b>Combat Gear</b><br>", "100",
-	array("DELETE", "; ", 
-		array("quality", "", "", "70"), 
-		array("repair", 0, "", "60", 0, array("Good", "Worn", "Broken")), 
-		array("is_equipped", 0, "", "60", "Yes", array("Yes", "No")), 
+		array("quality", "", "", "90"), 
+		array("count", "", "", "30"), 
+		array("is_equipped", 0, "", "72", "", array("Armour", "No", "Mount", "Storage")), 
 		array("is_deleted", 0, 0, 0)
 	)
 );
 $output->addEdit("<br>");
 
 // other gear
-$output->add("other", $view->characterTreasure[other], "Other Gear", "<b>Other Gear</b><br>", "100",
+$output->add("other", $view->characterTreasure[other], 0, "<b>Other</b><br>", "100",
 	array("DELETE", "; ", 
-		array("quality", "", "", "70"), 
-		array("repair", 0, "", "60", 0, array("Good", "Worn", "Broken")), 
-		array("is_equipped", 0, "", "60", "No", array("Melee", "Off-Hand", "Ranged", "Armour", "Yes", "No")), 
+		array("quality", "", "", "90"), 
+		array("count", "", "", "30"), 
+		array("is_equipped", 0, "", "72", "", array("Yes", "No", "Mount", "Storage", "Melee", "Off-Hand", "Ranged", "Armour")), 
 		array("is_deleted", 0, 0, 0)
 	)
 );
-$output->br();
 
 echo $output->dump(1);
 

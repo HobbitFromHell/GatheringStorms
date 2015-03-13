92ab3fbbef27e528d44659fcdf3a9cf1d8973f01
myronwyles <myronwyles@gmail.com>
2015-03-13 11:00:18 -0700

    Add religion field

--- character/description/view.php
+++ character/description/view.php
@@ -8,19 +8,23 @@ $output->add("id", $pkid, 0, 0);
 
 // description
 $output->add("", "<div class=\"statBlockDetail\">", "", "", "");
-$output->add("description", $view->characterDescription[description][name], "", "", "textarea");
+$output->add("description", $view->characterDescription['description']['name'], "", "", "textarea");
 $output->add("", "</div>", "", "", "");
 $output->br();
 
+// race
+$output->add("god_id", $view->characterDescription['description']['god'], "Religion", "Religion", "200");
+$output->br();
+
 // location
-$output->add("location", $view->characterDescription[description][location], "Location", "Location", "200");
-if($view->characterDescription[description][loc_name]) {
-	$output->addRead("(" . $view->characterDescription[description][loc_name] . ")");
+$output->add("location", $view->characterDescription['description']['location'], "Location", "Location", "200");
+if($view->characterDescription['description']['loc_name']) {
+	$output->addRead("(" . $view->characterDescription['description']['loc_name'] . ")");
 }
 $output->br();
 
 echo $output->dump(1);
 
-echo drawHexMap($varXCo, $varYCo, $varHexSize, $view->characterDescription[location]);
+echo drawHexMap($varXCo, $varYCo, $varHexSize, $view->characterDescription['location']);
 
 ?>

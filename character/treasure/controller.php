99e2039bd17c89979bbcc5ab7f8fba834d5b5a26
myronwyles <myronwyles@gmail.com>
2015-03-15 22:07:13 -0700

    magic rings; some fixes
    Magic rings and costs added.
    Fix to magic weapon (all are considered mwk)
    Fix to mwk settings (0 = no, 1 = yes, 2 = yes and cost already included)
    Fix to allow shield to appear as a weapon
    Add tool tip to show value and weight of item on mouse over.

--- character/treasure/controller.php
+++ character/treasure/controller.php
@@ -87,9 +87,11 @@ $j = DataConnector::selectQuery("
 	    AND ce.`is_deleted` != 'Yes'
 ");
 
+$tmpMaxDeflectionBonus = 0;
+
 // modify equipment based on repair, qualities, size; then assign to an equipment list
 while($j) {
-	$is_masterwork = 0;
+	$is_masterwork = 0; // 1 = make mwk; 2 = make mwk, cost included
 	$tmpAddedCost = 0;
 	$tmpBonus = 0;
 	$multiplier = 1;
@@ -114,9 +116,10 @@ while($j) {
 			$j['quality'] = "+" . $j['quality'];
 		}
 		// modify equipment based on magical bonus
+		$is_masterwork = 2; // All magic armor is also masterwork armor, p.461
 		$j['damage_mod'] = $tmpBonus;
 		$j['to_hit_mod'] = $tmpBonus;
-		$j['armour_hp'] += $tmpBonus * 5;
+		$j['armour_hp'] += ($tmpBonus - 1) * 5;
 	}
 	$j['quality_format'] = $j['quality'];
 
@@ -146,13 +149,170 @@ while($j) {
 		$j['armour_penalty'] *= 2;
 	}
 
+	// rings
+	if(strtolower($j['name']) == "ring") {
+		if(stripos($j['quality'], "protection") !== FALSE) {
+			if(stripos($j['quality'], "+1") !== FALSE) {
+				$j['armour_ac'] += 1;
+				$j['cost'] += 200000;
+			}
+			else if(stripos($j['quality'], "+2") !== FALSE) {
+				$j['armour_ac'] += 2;
+				$j['cost'] += 800000;
+			}
+			else if(stripos($j['quality'], "+3") !== FALSE) {
+				$j['armour_ac'] += 3;
+				$j['cost'] += 1800000;
+			}
+			else if(stripos($j['quality'], "+4") !== FALSE) {
+				$j['armour_ac'] += 4;
+				$j['cost'] += 3200000;
+			}
+			else if(stripos($j['quality'], "+5") !== FALSE) {
+				$j['armour_ac'] += 5;
+				$j['cost'] += 5000000;
+			}
+		}
+		if(stripos($j['quality'], "feather falling") !== FALSE) {
+			$j['cost'] += 220000;
+		}
+		if(stripos($j['quality'], "climbing") !== FALSE) {
+			if(stripos($j['quality'], "improved") !== FALSE) {
+				$j['cost'] += 1000000;
+			}
+			else {
+				$j['cost'] += 250000;
+			}
+		}
+		if(stripos($j['quality'], "jumping") !== FALSE) {
+			if(stripos($j['quality'], "improved") !== FALSE) {
+				$j['cost'] += 1000000;
+			}
+			else {
+				$j['cost'] += 250000;
+			}
+		}
+		if(stripos($j['quality'], "swimming") !== FALSE) {
+			if(stripos($j['quality'], "improved") !== FALSE) {
+				$j['cost'] += 1000000;
+			}
+			else {
+				$j['cost'] += 250000;
+			}
+		}
+		if(stripos($j['quality'], "sustenance") !== FALSE) {
+			$j['cost'] += 250000;
+		}
+		if(stripos($j['quality'], "counterspells") !== FALSE) {
+			$j['cost'] += 400000;
+		}
+		if(stripos($j['quality'], "Mind shielding") !== FALSE) {
+			$j['cost'] += 800000;
+		}
+		if(stripos($j['quality'], "Force shield") !== FALSE) {
+			$j['cost'] += 850000;
+		}
+		if(stripos($j['quality'], "the Ram") !== FALSE) {
+			$j['cost'] += 860000;
+		}
+		if(stripos($j['quality'], "Animal friendship") !== FALSE) {
+			$j['cost'] += 1080000;
+		}
+		if(stripos($j['quality'], "Energy resistance") !== FALSE) {
+			if(stripos($j['quality'], "minor") !== FALSE) {
+				$j['cost'] += 1200000;
+			}
+			else if(stripos($j['quality'], "major") !== FALSE) {
+				$j['cost'] += 2800000;
+			}
+			else if(stripos($j['quality'], "greater") !== FALSE) {
+				$j['cost'] += 4400000;
+			}
+		}
+		if(stripos($j['quality'], "Chameleon") !== FALSE) {
+			$j['cost'] += 1270000;
+		}
+		if(stripos($j['quality'], "Water walking") !== FALSE) {
+			$j['cost'] += 1500000;
+		}
+		if(stripos($j['quality'], "Spell storing") !== FALSE) {
+			if(stripos($j['quality'], "minor") !== FALSE) {
+				$j['cost'] += 1800000;
+			}
+		}
+		if(stripos($j['quality'], "Invisibility") !== FALSE) {
+			$j['cost'] += 2000000;
+		}
+		if(stripos($j['quality'], "Wizardry") !== FALSE) {
+			if(stripos($j['quality'], "(I)") !== FALSE) {
+				$j['cost'] += 2000000;
+			}
+			else if(stripos($j['quality'], "(II)") !== FALSE) {
+				$j['cost'] += 4000000;
+			}
+			else if(stripos($j['quality'], "(III)") !== FALSE) {
+				$j['cost'] += 7000000;
+			}
+			else if(stripos($j['quality'], "(IIII)") !== FALSE) {
+				$j['cost'] += 10000000;
+			}
+			else if(stripos($j['quality'], "(IV)") !== FALSE) {
+				$j['cost'] += 10000000;
+			}
+		}
+		if(stripos($j['quality'], "Evasion") !== FALSE) {
+			$j['cost'] += 2500000;
+		}
+		if(stripos($j['quality'], "X-ray vision") !== FALSE) {
+			$j['cost'] += 2500000;
+		}
+		if(stripos($j['quality'], "Blinking") !== FALSE) {
+			$j['cost'] += 2700000;
+		}
+		if(stripos($j['quality'], "Freedom") !== FALSE) {
+			$j['cost'] += 4000000;
+		}
+		if(stripos($j['quality'], "Friend shield") !== FALSE) {
+			$j['cost'] += 2500000;
+		}
+		if(stripos($j['quality'], "Shooting stars") !== FALSE) {
+			$j['cost'] += 5000000;
+		}
+		if(stripos($j['quality'], "Spell storing") !== FALSE) {
+			if(stripos($j['quality'], "major") !== FALSE) {
+				$j['cost'] += 20000000;
+			}
+			else {
+				$j['cost'] += 5000000;
+			}
+		}
+		if(stripos($j['quality'], "Telekinesis") !== FALSE) {
+			$j['cost'] += 7500000;
+		}
+		if(stripos($j['quality'], "Regeneration") !== FALSE) {
+			$j['cost'] += 9000000;
+		}
+		if(stripos($j['quality'], "Spell turning") !== FALSE) {
+			$j['cost'] += 10000000;
+		}
+		if(stripos($j['quality'], "Three wishes") !== FALSE) {
+			$j['cost'] += 12000000;
+		}
+		if(stripos($j['quality'], "Djinni calling") !== FALSE) {
+			$j['cost'] += 12500000;
+		}
+		if(stripos($j['quality'], "Elemental command") !== FALSE) {
+			$j['cost'] += 20000000;
+		}
+	}
+
 	// modify for adamantine item (p.154)
 	if(stripos($j['quality'], "adamantine") !== FALSE or 
 	   stripos($j['quality'], "dwarven")    !== FALSE) {
-		$is_masterwork = 1;
+		$is_masterwork = 2;
 		if($j['melee_category']) {
 			// weapon
-			$tmpAddedCost += 300000 - 20000;
+			$tmpAddedCost += 300000;
 		}
 		else if($j['armour_category']) {
 			// Armor made of adamantine has one-third more hit points than normal.
@@ -160,7 +320,7 @@ while($j) {
 			if($j['armour_category'] == "Light") {
 				// Armor made from adamantine grants its wearer damage reduction of 1/— if it’s light armor.
 				// house rule: the dr is doubled
-				$tmpAddedCost += 500000 - 20000;
+				$tmpAddedCost += 500000;
 				$j['armour_hardness'] += 2;
 				if($j['armour_dc'] < 5) {
 					// Light armour that is actually a limited form of medium armour
@@ -170,7 +330,7 @@ while($j) {
 			if($j['armour_category'] == "Medium") {
 				// Armor made from adamantine grants its wearer damage reduction of 2/— if it’s medium armor.
 				// house rule: the dr is doubled
-				$tmpAddedCost += 1000000 - 20000;
+				$tmpAddedCost += 1000000;
 				$j['armour_hardness'] += 4;
 				if($j['armour_dc'] < 5) {
 					// Medium armour that is actually a limited form of heavy armour
@@ -180,13 +340,13 @@ while($j) {
 			if($j['armour_category'] == "Heavy") {
 				// Armor made from adamantine grants its wearer damage reduction of 3/— if it’s heavy armor.
 				// house rule: the dr is doubled
-				$tmpAddedCost += 1500000 - 20000;
+				$tmpAddedCost += 1500000;
 				$j['armour_hardness'] += 6;
 			}
 		}
 		else {
 			// ammunition
-			$tmpAddedCost += (6000 - 600);
+			$tmpAddedCost += 6000;
 		}
 	}
 
@@ -220,7 +380,7 @@ while($j) {
 	// modify for mithral item (p.154/155)
 	if(stripos($j['quality'], "mithral") !== FALSE or 
 	   stripos($j['quality'], "elven") !== FALSE) {
-		$is_masterwork = 1;
+		$is_masterwork = 2;
 		if($j['armour_category']) {
 			// **house rule change: treat like adamantine, granting dr 1/--, 2/-- or 3/--
 			// Spell failure chances for armors and shields made from mithral are decreased by 10%, maximum Dexterity bonuses are increased by 2, and armor check penalties are decreased by 3 (to a minimum of 0).
@@ -228,10 +388,10 @@ while($j) {
 			$j['armour_penalty'] = max(0, $j['armour_penalty'] - 2);
 			$j['armour_spell_failure'] = max(0, $j['armour_spell_failure'] - 10);
 			if($j['armour_category'] == "Shield") {
-				$tmpAddedCost += 100000 - 20000;
+				$tmpAddedCost += 100000;
 			}
 			if($j['armour_category'] == "Light") {
-				$tmpAddedCost += 100000 - 20000;
+				$tmpAddedCost += 100000;
 				$j['armour_hardness'] += 1;
 				if($j['armour_dc'] < 5) {
 					// Light armour that is actually a limited form of medium armour
@@ -241,7 +401,7 @@ while($j) {
 			if($j['armour_category'] == "Medium") {
 				// Most mithral armors are one category lighter for purposes of movement and other limitations.
 				$j['armour_category'] = "Light";
-				$tmpAddedCost += 400000 - 20000;
+				$tmpAddedCost += 400000;
 				$j['armour_hardness'] += 2;
 				if($j['armour_dc'] < 5) {
 					// Medium armour that is actually a limited form of heavy armour
@@ -251,7 +411,7 @@ while($j) {
 			if($j['armour_category'] == "Heavy") {
 				// Most mithral armors are one category lighter for purposes of movement and other limitations.
 				$j['armour_category'] = "Medium";
-				$tmpAddedCost += 900000 - 20000;
+				$tmpAddedCost += 900000;
 				$j['armour_hardness'] += 3;
 			}
 		}
@@ -290,7 +450,9 @@ while($j) {
 			  "<span title='+1 enhancement bonus on damage rolls'>MWK</span>",
 			  $j['quality_format']);
 			// **house rule change: all mwk items cost 200 gp extra
-			$tmpAddedCost += 20000;
+			if($is_masterwork == 1) {
+				$tmpAddedCost += 20000;
+			}
 		}
 		else if($j['armour_category']) {
 			// "...its armor check penalty is lessened by 1."
@@ -301,11 +463,15 @@ while($j) {
 			  "<span title='Armour check penalty lessened by 1; +5 enhancement bonus on armour hp'>MWK</span>",
 			  $j['quality_format']);
 			// **house rule change: all mwk items cost 200 gp extra
-			$tmpAddedCost += 20000;
+			if($is_masterwork == 1) {
+				$tmpAddedCost += 20000;
+			}
 		}
 		else {
 			// ammunition costs 6 gp extra
-			$tmpAddedCost += 600;
+			if($is_masterwork == 1) {
+				$tmpAddedCost += 600;
+			}
 		}
 	}
 
@@ -314,38 +480,38 @@ while($j) {
 		if(stripos($j['quality'], "glamered") !== FALSE) {
 			$tmpAddedCost += 270000;
 		}
-		if(stripos($j['quality'], "greater slick") !== FALSE or
-		   stripos($j['quality'], "slick, greater") !== FALSE) {
-			$tmpAddedCost += 3375000;
+		if(stripos($j['quality'], "slick") !== FALSE) {
+			if(stripos($j['quality'], "greater") !== FALSE) {
+				$tmpAddedCost += 3375000;
+			}
+			else if(stripos($j['quality'], "improved") !== FALSE) {
+				$tmpAddedCost += 1500000;
+			}
+			else {
+				$tmpAddedCost += 375000;
+			}
 		}
-		else if(stripos($j['quality'], "improved slick") !== FALSE or
-		   stripos($j['quality'], "slick, improved") !== FALSE) {
-			$tmpAddedCost += 1500000;
+		if(stripos($j['quality'], "shadow") !== FALSE) {
+			if(stripos($j['quality'], "greater") !== FALSE) {
+				$tmpAddedCost += 3375000;
+			}
+			else if(stripos($j['quality'], "improved") !== FALSE) {
+				$tmpAddedCost += 1500000;
+			}
+			else {
+				$tmpAddedCost += 375000;
+			}
 		}
-		else if(stripos($j['quality'], "slick") !== FALSE) {
-			$tmpAddedCost += 375000;
-		}
-		if(stripos($j['quality'], "greater shadow") !== FALSE or
-		   stripos($j['quality'], "shadow, greater") !== FALSE) {
-			$tmpAddedCost += 3375000;
-		}
-		else if(stripos($j['quality'], "improved shadow") !== FALSE or
-		   stripos($j['quality'], "shadow, improved") !== FALSE) {
-			$tmpAddedCost += 1500000;
-		}
-		else if(stripos($j['quality'], "shadow") !== FALSE) {
-			$tmpAddedCost += 375000;
-		}
-		if(stripos($j['quality'], "energy resistance, greater") !== FALSE or
-		   stripos($j['quality'], "greater energy resistance") !== FALSE) {
-			$tmpAddedCost += 6600000;
-		}
-		else if(stripos($j['quality'], "energy resistance, improved") !== FALSE or
-		   stripos($j['quality'], "improved energy resistance") !== FALSE) {
-			$tmpAddedCost += 4200000;
-		}
-		else if(stripos($j['quality'], "energy resistance") !== FALSE) {
-			$tmpAddedCost += 1800000;
+		if(stripos($j['quality'], "energy resistance") !== FALSE) {
+			if(stripos($j['quality'], "greater") !== FALSE) {
+				$tmpAddedCost += 6600000;
+			}
+			else if(stripos($j['quality'], "improved") !== FALSE) {
+				$tmpAddedCost += 4200000;
+			}
+			else {
+				$tmpAddedCost += 1800000;
+			}
 		}
 		if(stripos($j['quality'], "etherealness") !== FALSE) {
 			$tmpAddedCost += 4900000;
@@ -365,32 +531,33 @@ while($j) {
 		   stripos($j['quality'], "fortification, heavy") !== FALSE) {
 			$tmpBonus += 5;
 		}
-		if(stripos($j['quality'], "light deflection") !== FALSE or
-		   stripos($j['quality'], "deflection, light") !== FALSE) {
-			$tmpBonus += 1;
-			$j['armour_ac'] += 1;
+		if(stripos($j['quality'], "deflection") !== FALSE) {
+			if(stripos($j['quality'], "heavy") !== FALSE) {
+				$tmpBonus += 3;
+				$tmpMaxDeflectionBonus = 3;
+			}
+			else if(stripos($j['quality'], "moderate") !== FALSE) {
+				$tmpBonus += 2;
+				$tmpMaxDeflectionBonus = 2;
+			}
+			else {
+				$tmpBonus += 1;
+				$tmpMaxDeflectionBonus = 1;
+			}
 		}
-		if(stripos($j['quality'], "moderate deflection") !== FALSE or
-		   stripos($j['quality'], "deflection, moderate") !== FALSE) {
-			$tmpBonus += 2;
-			$j['armour_ac'] += 2;
-		}
-		if(stripos($j['quality'], "heavy deflection") !== FALSE or
-		   stripos($j['quality'], "deflection, heavy") !== FALSE) {
-			$tmpBonus += 3;
-			$j['armour_ac'] += 3;
-		}
-		if(stripos($j['quality'], "spell resistance (13)") !== FALSE) {
-			$tmpBonus += 2;
-		}
-		if(stripos($j['quality'], "spell resistance (15)") !== FALSE) {
-			$tmpBonus += 3;
-		}
-		if(stripos($j['quality'], "spell resistance (17)") !== FALSE) {
-			$tmpBonus += 4;
-		}
-		if(stripos($j['quality'], "spell resistance (19)") !== FALSE) {
-			$tmpBonus += 5;
+		if(stripos($j['quality'], "spell resistance") !== FALSE) {
+			if(stripos($j['quality'], "(13)") !== FALSE) {
+				$tmpBonus += 2;
+			}
+			if(stripos($j['quality'], "(15)") !== FALSE) {
+				$tmpBonus += 3;
+			}
+			if(stripos($j['quality'], "(17)") !== FALSE) {
+				$tmpBonus += 4;
+			}
+			if(stripos($j['quality'], "(19)") !== FALSE) {
+				$tmpBonus += 5;
+			}
 		}
 		if(stripos($j['quality'], "ghost touch") !== FALSE) {
 			$tmpBonus += 3;
@@ -527,16 +694,26 @@ while($j) {
 		}
 	}
 	switch($tmpBonus) {
-		case 1:   $tmpAddedCost += $multiplier * 100000; break;
-		case 2:   $tmpAddedCost += $multiplier * 400000; break;
-		case 3:   $tmpAddedCost += $multiplier * 900000; break;
-		case 4:   $tmpAddedCost += $multiplier * 1600000; break;
-		case 5:   $tmpAddedCost += $multiplier * 2500000; break;
-		case 6:   $tmpAddedCost += $multiplier * 3600000; break;
-		case 7:   $tmpAddedCost += $multiplier * 4900000; break;
-		case 8:   $tmpAddedCost += $multiplier * 6400000; break;
-		case 9:   $tmpAddedCost += $multiplier * 8100000; break;
-		case 10:  $tmpAddedCost += $multiplier * 10000000; break;
+		case 1:  $tmpAddedCost += $multiplier * 100000;
+		         break;
+		case 2:  $tmpAddedCost += $multiplier * 400000;
+		         break;
+		case 3:  $tmpAddedCost += $multiplier * 900000;
+		         break;
+		case 4:  $tmpAddedCost += $multiplier * 1600000;
+		         break;
+		case 5:  $tmpAddedCost += $multiplier * 2500000;
+		         break;
+		case 6:  $tmpAddedCost += $multiplier * 3600000;
+		         break;
+		case 7:  $tmpAddedCost += $multiplier * 4900000;
+		         break;
+		case 8:  $tmpAddedCost += $multiplier * 6400000;
+		         break;
+		case 9:  $tmpAddedCost += $multiplier * 8100000;
+		         break;
+		case 10: $tmpAddedCost += $multiplier * 10000000;
+		         break;
 	}
 
 	// add cost for enchantments, multiply by number of items
@@ -561,31 +738,55 @@ while($j) {
 	if(substr($j['name'], 0, 6) == "Unique") {
 		$j['name'] = "";
 	}
-	// read-only
+
+	// calculate description
+	$tmpCost = $j['cost'];
 	if($j['count'] > 1) {
 		// show number of items in name
 		$j['name'] .= " (" . $j['count'] . ")";
-	}
-	if($j['is_equipped'] == "No") {
-		$view->otherGear .= $j['quality_format'] . " " . $j['name'] . " ; ";
 		$view->total_weight += $j['weight'] * $j['count'];
+		$tmpCost *= $j['count'];
+	}
+	if($tmpCost < 99 and $tmpCost % 10 != 0) {
+		$tmpCost .= " cp; ";
+	}
+	else if($tmpCost < 999 and $tmpCost % 100 != 0) {
+		$tmpCost = floor($tmpCost / 10) . " sp; ";
+	}
+	else {
+		$tmpCost = floor($tmpCost / 100) . " gp; ";
+	}
+	$tmpCost .= $j['weight'] * $j['count'];
+	$tmpCost .= " lbs.";
+
+	// read-only
+	$tmpDescription = "<span title='{$tmpCost}'>{$j['quality_format']} {$j['name']}</span> ; ";
+	if($j['is_equipped'] == "No") {
+		$view->otherGear .= $tmpDescription;
 	}
 	else if($j['is_equipped'] == "Mount") {
-		$view->mountGear .= $j['quality_format'] . " " . $j['name'] . " ; ";
+		$view->mountGear .= $tmpDescription;
 		// do not calculate weight for equipment on a mount
 	}
 	else if($j['is_equipped'] == "Storage") {
-		$view->storedGear .= $j['quality_format'] . " " . $j['name'] . " ; ";
+		$view->storedGear .= $tmpDescription;
 		// do not calculate weight for stored equipment
 	}
 	else {
-		$view->combatGear .= $j['quality_format'] . " " . $j['name'] . " ; ";
+		$view->combatGear .= $tmpDescription;
 		$view->total_weight += $j['weight'] * $j['count'];
-		if($j['armour_category'] == "Shield") {
-			$view->characterTreasure['equipped']['shield'] = $j;
+		if($j['armour_ac'] and $j['armour_ac'] > $tmpMaxDeflectionBonus) {
+			$tmpMaxDeflectionBonus = $j['armour_ac'];
 		}
-		else if ($j['is_equipped'] == "Armour") {
-			$view->characterTreasure['equipped']['armour'] = $j;
+
+		// assign combat gear to communicate with main page
+		if($j['is_equipped'] == "Armour") {
+			if($j['armour_category'] == "Shield") {
+				$view->characterTreasure['equipped']['shield'] = $j;
+			}
+			else if ($j['is_equipped'] == "Armour") {
+				$view->characterTreasure['equipped']['armour'] = $j;
+			}
 		}
 		else if ($j['is_equipped'] == "Melee") {
 			$view->characterTreasure['equipped']['melee'] = $j;
@@ -613,7 +814,6 @@ $j = DataConnector::selectQuery("
 	         e.`melee_category`  AS `melee_category`,
 	         e.`ranged_category` AS `ranged_category`
 	   FROM t_equipment e
-	  WHERE e.`id` < 281
 	  ORDER BY e.`name`
 ");
 while($j) {
@@ -643,6 +843,7 @@ $js_array = json_encode($view->characterTreasure['equipped']['armour']);
 echo "charSheet.armour = {$js_array}\n";
 $js_array = json_encode($view->characterTreasure['equipped']['shield']);
 echo "charSheet.shield = {$js_array}\n";
+echo "charSheet.adjustments.original.acDeflect = {$tmpMaxDeflectionBonus}\n";
 echo "buildSection('Feats', 'charLevel=' + charSheet.getNumber('charLevel') 
                           + '&casterLevel=' + charSheet.buildCasterLevel() 
                           + '&race=' + charSheet.getValue('race') 
